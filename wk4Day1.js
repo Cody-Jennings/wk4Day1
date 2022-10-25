8959828558af0746b6c28749512107d2f73f10fb
Cody Jennings <cojack91@gmail.com>
2022-10-25 13:15:58 -0400

    first commit

---
+++ wk4Day1.js
@@ -0,0 +1,79 @@
+//traffic light code wars
+// const changingStoplight = (current) => {
+//     if (current == "green"){
+//         return "yellow"
+//     } else if (current == "yellow") {
+//         return "red"
+//     } else {
+//         return "green"
+//     }
+// }
+// console.log(changingStoplight("yellow"))
+
+// how many light sabers do you own
+
+// const owningLightsabers = (name) => {
+//     if(name === "Zach") {
+//         return 18      //return name === "Zach" ? 18 : 0
+//     } else {
+//         return 0
+//     }
+// }
+// console.log(owningLightsabers("Cody"))
+
+// const owningLightsabers = (name) => name === "Zach" ? 18 : 0
+
+// console.log(owningLightsabers("Gary"))
+
+
+
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+// function 1
+
+const logNumbers = () => {
+    for(let i = 1; i <= 20; i++) {
+        console.log(i)
+    }
+}
+logNumbers();
+
+////////////////////////////////
+// Get Even
+////////////////////////////////
+
+
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
