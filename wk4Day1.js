21bdbe76838391d1910988c4f7d3072121de57b4
Cody Jennings <cojack91@gmail.com>
2022-10-25 13:40:51 -0400

    second function

--- wk4Day1.js
+++ wk4Day1.js
@@ -42,16 +42,36 @@
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-const logEvenNumbers = () => {
-    for(let i = 0; i <= 200; i +=2) {
-        console.log(i)
-    }
-}
-logEvenNumbers();
+// const logEvenNumbers = () => {
+//     for(let i = 0; i <= 200; i +=2) {
+//         console.log(i)
+//     }
+// }
+// logEvenNumbers();
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+const getMyNumbers = () => {
+    let num;
+    for(let i=1; i<101; i++){
+        num=i;
+    
+    if(num%3===0 && num%5===0){
+        console.log(`FizzBuzz`);
+    }
+    else if(num%3===0){
+        console.log(`Fizz`);
+    }
+    else if(num%5===0){
+        console.log(`Buzz`);
+    }
+    else {
+        console.log(num);
+    }
+}
+}
+getMyNumbers();
 
 
 ////////////////////////////////
